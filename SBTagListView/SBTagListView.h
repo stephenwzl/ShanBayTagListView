a955366cca72557eaeb6a337dbc11f2e068342ff
王志龙 <991405736qq@gmail.com>
2015-09-16 18:38:20 +0800

    commit

--- SBTagListView/SBTagListView.h
+++ SBTagListView/SBTagListView.h
@@ -8,6 +8,40 @@
 
 #import <UIKit/UIKit.h>
 
+typedef enum :NSInteger{
+    SBTagTypeNormal = 0,
+    SBTagTypeError,
+    SBTagTypeSuccess,
+}SBTagType;
+
+@protocol SBTagListViewDelegate <NSObject>
+
+@optional
+- (void)didSelectTagAtIndex:(NSUInteger)index;
+
+@end
+
+
+
+@interface SBTag : UIButton
+
+@property (copy, nonatomic) NSString *text;
+@property (assign, nonatomic) NSUInteger index;
+
+- (instancetype)initWithText:(NSString *)text;
+- (void)setTagType:(SBTagType)type;
+@end
+
+
+
 @interface SBTagListView : UIView
+
+@property (strong, nonatomic) NSMutableArray *contentArray;
+@property (strong, nonatomic) NSMutableArray *tagsArray;
+@property (assign, nonatomic) id<SBTagListViewDelegate> delegate;
+
+- (instancetype)initWithWidth:(CGFloat)width contentArray:(NSMutableArray *)array;
+
+- (SBTag *)desequeseTagAtIndex:(NSUInteger)index;
 
 @end
--- SBTagListView/SBTagListView.m
+++ SBTagListView/SBTagListView.m
@@ -8,6 +8,37 @@
 
 #import "SBTagListView.h"
 
+
+
 @implementation SBTagListView
+
+- (instancetype)initWithWidth:(CGFloat)width contentArray:(NSMutableArray *)array {
+    self = [super init];
+    if (self) {
+        self.contentArray = array;
+    }
+    return self;
+}
+
+- (SBTag *)desequeseTagAtIndex:(NSUInteger)index {
+    for (SBTag *tag in self.tagsArray) {
+        if (tag.index == index) {
+            return tag;
+        }
+    }
+    return nil;
+}
+@end
+
+@implementation SBTag
+
+- (instancetype)initWithText:(NSString *)text {
+    CGSize contentSize = [text boundingRectWithSize:CGSizeMake(FLT_MAX, 30) options:NSStringDrawingUsesFontLeading attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:17]} context:nil].size;
+    self = [super initWithFrame:CGRectMake(0, 0, contentSize.width + 20, contentSize.height)];
+    if (self) {
+        self.text = text;
+    }
+    return self;
+}
 
 @end
